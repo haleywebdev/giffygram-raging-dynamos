d1b1711c71bd79e8e964a7ab3cd491c682dc245b
AriDaron <arielle.daron@asurion.com>
2021-11-10 13:57:24 -0600

    added new post html

--- src/scripts/GiffyGram.js
+++ src/scripts/GiffyGram.js
@@ -1,10 +1,11 @@
+import { PostForm } from "./feed/PostEntry.js"
+
 export const GiffyGram = () => {
 
     // Show main main UI
     return `
     <h2> this is a test </h2> 
-    //! Function that has the HTML interpolation for the NavBar
-    //! Function that has the giffygram__feed HTML interpolation which includes the array of posts and the "Have a gif to Post? button included. 
-    //! Function that has the footer HTML interpolation for the Posts since dropdown, posts by user dropdown, and dhow only favorites checkbox. 
+    ${PostForm()}
+    
     `
 }
