a83e8f82e89d54a715131141da3541e73fe12a58
lacizeidler <laci.zeidler@gmail.com>
2021-11-10 09:50:10 -0600

    main module edited

--- src/scripts/GiffyGram.js
+++ src/scripts/GiffyGram.js
@@ -1,5 +1,9 @@
 export const GiffyGram = () => {
 
     // Show main main UI
-    return `<h1>Giffygram</h1>`
+    return `
+    //! Function that has the HTML interpolation for the NavBar
+    //! Function that has the giffygram__feed HTML interpolation which includes the array of posts and the "Have a gif to Post? button included. 
+    //! Function that has the footer HTML interpolation for the Posts since dropdown, posts by user dropdown, and dhow only favorites checkbox. 
+    `
 }
